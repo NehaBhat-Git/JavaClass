186891fb9358bda1f7d86347679157a056a0c766
NehaBhat-Git <89778970+NehaBhat-Git@users.noreply.github.com>
2024-03-27 11:05:24 +0530

    PrintSizeRange

---
+++ Class_2/PrintSizeRange.java
@@ -0,0 +1,39 @@
+class PrintSizeRange{
+        public static void main(String[] args){
+          System.out.println("\nBYTE MIN Value: "+Byte.MIN_VALUE);
+          System.out.println("BYTE MAX VALUE: "+Byte.MAX_VALUE);  
+          System.out.println("*******************************************\n");
+
+          System.out.println("CHARACTER MIN VALUE: "+(int)Character.MIN_VALUE);
+          System.out.println("CHARACTER MIN VALUE: "+(int)Character.MAX_VALUE);
+          System.out.println("******************************************\n");
+
+          System.out.println("SHORT MIN VALUE: "+Short.MIN_VALUE);
+          System.out.println("SHORT MAX VALUE: "+Short.MAX_VALUE);
+          System.out.println("*******************************************\n");
+          
+          System.out.println("INTEGER MIN Value: "+Integer.MIN_VALUE);
+          System.out.println("INTEGER MAX VALUE: "+Integer.MAX_VALUE);  
+          System.out.println("*******************************************\n");
+
+          System.out.println("LONG MIN VALUE: "+Long.MIN_VALUE);
+          System.out.println("LONG MAX VALUE: "+Long.MAX_VALUE);
+          System.out.println("*******************************************\n");
+          
+          System.out.println("FLOAT MIN VALUE: "+Float.MIN_VALUE);
+          System.out.println("FLOAT MAX VALUE: "+Float.MAX_VALUE);
+          System.out.println("*******************************************\n");
+
+          System.out.println("DOUBLE MIN VALUE: "+Double.MIN_VALUE);
+          System.out.println("DOUBLE MAX VALUE: "+Double.MAX_VALUE);
+          System.out.println("*******************************************\n");
+/*
+          WILL PROVIDE THE ERROR
+          System.out.println("BOOLEAN MIN VALUE: "+Boolean.MIN_VALUE);
+          System.out.println("BOOLEAN MAX VALUE: "+Boolean.MAX_VALUE);
+          System.out.println("*******************************************\n"); */
+
+    
+      
+        }
+}
